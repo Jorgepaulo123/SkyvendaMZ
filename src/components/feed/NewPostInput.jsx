ab65b5a5738837d92dc39a1522aed48b5496c538
Ghost404 <diguijoaquim@users.noreply.github.com>
2025-09-29 16:05:48 +0200

    ola

--- src/components/feed/NewPostInput.jsx
+++ src/components/feed/NewPostInput.jsx
@@ -26,7 +26,7 @@ export default function NewPostInput() {
   };
 
   return (
-    <div className="bg-white border-b border-gray-200 px-4 md:px-6 pb-3 pt-1">
+    <div className="bg-white border-b border-gray-200 px-4 md:px-6 pb-2 pt-1">
       <div className="flex items-center space-x-3">
         {/* Avatar */}
         <div className="flex-shrink-0">
